db2df6bfd3925d7854039b56e5ee460e39d5c9c6
Maciej Owcarz <maciejowcarz@Maciejs-MacBook-Pro.local>
2025-04-02 13:40:42 +0200

    progress;

---
+++ src/views/DynamicView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Dynamic</h1>
+    <div>{{ useRoute().params.uuid }}</div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+</script>
